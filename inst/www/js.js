9bb18c17a5a530ca817ec246c60d2d546ae69f04
slfan2013 <fansili2013@gmail.com>
2016-10-18 13:56:58 -0700

    a

--- inst/www/js.js
+++ inst/www/js.js
@@ -1,2 +1,12 @@
 (function(){
+   $('#upload').change(upload);
+  function upload(){
+  var req=ocpu.call("upload",{
+    path:$("#upload")[0].files[0]
+  },function(session){
+    console.log(session)
+  }).fail(function() {alert("Error: " + req.responseText);})
+}
+
+
 })()
